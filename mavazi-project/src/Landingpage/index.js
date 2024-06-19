eb9ba79b909ae18fe789cc5f4f874ec086f0e0d0
Gitahi-Moureen <moureengitahiwangari@gmail.com>
2024-06-19 07:02:29 +0300

    Add homepage

---
+++ mavazi-project/src/Landingpage/index.js
@@ -0,0 +1,48 @@
+import './index.css'
+const Landingpage = () =>{
+    return(
+
+    <div>
+
+        <div id='landing'>
+           
+
+            <div id="second">
+                <div id='sautisoul'>
+                    <img src="/Images/image (5).png"/>
+                </div>
+                <div  id='landingtext'>
+                    <h4>Sustainable fashion in not a trend,but the future,<br></br> driven by the need to address environmental and social issues <br></br> while meeting growing consumer demand for ethical and eco-friendly practices.</h4>
+                    
+                </div>
+
+
+            </div>
+            <div id='featured'>
+                <div id='fb'>
+                    <h4>Featured Brands</h4>
+                </div>
+
+                <div id='BrandsF'>
+                   
+                    <img src='/Images/zara.png'className='zara'/>
+
+                   <img src='/Images/Katush.png'className='katush'/>
+
+                    <img src='/Images/lilabare.png' className='lilabare'/>
+
+                    <img src='/Images/Mango.png' className='mango'/>
+                   
+                    
+                </div>
+
+
+            </div>
+            
+
+
+        </div>
+    </div>
+    );
+}
+export default Landingpage
