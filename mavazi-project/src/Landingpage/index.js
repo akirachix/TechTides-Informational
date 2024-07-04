adee026c401f29cc298eafddf017b26307a7864c
Gitahi-Moureen <moureengitahiwangari@gmail.com>
2024-07-04 16:43:05 +0300

    Add files

--- mavazi-project/src/Landingpage/index.js
+++ mavazi-project/src/Landingpage/index.js
@@ -8,8 +8,8 @@ const Landingpage = () =>{
            
 
             <div id="second">
-                <div id='sautisoul'>
-                    <img src="/Images/image (5).png"/>
+                <div>
+                    <img src="/Images/sautiSoulf.jpg" alt ='soul' className='soul'/>
                 </div>
                 <div  id='landingtext'>
                     <h4>Sustainable fashion in not a trend,but the future,<br></br> driven by the need to address environmental and social issues <br></br> while meeting growing consumer demand for ethical and eco-friendly practices.</h4>
@@ -19,17 +19,15 @@ const Landingpage = () =>{
 
             </div>
             <div id='featured'>
-                <div id='fb'>
-                    <h4>Featured Brands</h4>
+                <div id='featuredbrands'>
+                    <h4>FEATURED BRANDS</h4>
                 </div>
 
-                <div id='BrandsF'>
+                <div id='brands'>
                    
                     <img src='/Images/zara.png'className='zara'/>
 
-                   <img src='/Images/Katush.png'className='katush'/>
-
-                    <img src='/Images/lilabare.png' className='lilabare'/>
+                    <img src='/Images/lilabare-removebg-preview.png' className='lilabare'/>
 
                     <img src='/Images/Mango.png' className='mango'/>
                    
