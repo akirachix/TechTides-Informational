6f043be2a2e2163581065c2f01e3d301df5ce415
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-07-06 22:56:25 +0300

    made changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Brands from './Brands';
+
+
+function App() {
+return (
+ <div>
+   <Brands/>
+     
+ </div>
+);
+}
+
+
+export default App;
+
+
---
+++ src/Brands/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+const Brands = () => {
+   return(
+    <div>
+        <h1 className="header">Brands</h1>
+       <div className="brands">
+       <div className='zara'>
+       <img src="/images/zar.png" className='label'/>
+       <img src="/images/za.jpeg" className='img'/>
+       <button className='overlay-text' type="Learn more">Learn more...</button>
+       </div>
+       <div className='katush'>
+       <img src="/images/katush.png" className='label'/>
+       <img src="/images/278005493_161884689602937_2756535273486692160_n.jpg" className='img'/>
+       <button className='overlay-text-2' type="Learn more">Learn more...</button>
+       </div>
+       <div className='lilabare'>
+       <img src="/images/lilabar.png" className='label'/>
+       <img src="/images/lilabare.jpeg" className='img'/>
+       <button className='overlay-text-3' type="Learn more">Learn more...</button>
+       </div>
+       <div className='mango'>
+       <img src="/images/mango.png" className='label'/>
+       <img src="/images/mango.jpeg" className='img'/>
+       <button className='overlay-text-4' type="Learn more">Learn more...</button>
+       </div>
+   </div>
+   </div>
+   );
+}
+export default Brands;
+
+
