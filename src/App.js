8a3c14e26ebb035488823c450addf60946246850
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-07-15 06:49:02 +0300

    made changes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+import Landingpage from './Landingpage'
+import Footer from './Footer'
+import Brands from './Brands';
+
+function App() {
+  return (
+    <div>
+      <Landingpage/>
+      <Brands/>
+      <Footer/>
+
+     
+    </div>
+  );
+  } 
+export  default App;
---
+++ src/Brands/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+const Brands = () => {
+   return(
+    <div>
+        <h1 className="header">Brands</h1>
+       <div className="brand">
+       <div className='zari'>
+       <img src="/Images/zara.png" className='label'/>
+       <img src="/Images/zaraBrand.webp" className='img'/>
+       <button className='overlay-text-zara' type="button">Learn more...</button>
+       </div>
+       <div className='katushi'>
+       <img src="/Images/Katush.png" className='label'/>
+       <img src="/Images/katushBrand.jpg" className='img'/>
+       <button className='overlay-text-katush' type="button">Learn more...</button>
+       </div>
+       <div className='lilabar'>
+       <img src="/Images/lilabare.png" className='label'/>
+       <img src="/Images/lilabareBrand.png" className='img'/>
+       <button className='overlay-text-lilabare' type="button">Learn more...</button>
+       </div>
+       <div className='mangos'>
+       <img src="/Images/Mango.png" className='label'/>
+       <img src="/Images/mangoBrand.jpg" className='img'/>
+       <button className='overlay-text-mango' type="button">Learn more...</button>
+       </div>
+   </div>
+   </div>
+   );
+}
+export default Brands;
